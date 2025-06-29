03d0594e42d682dcaf161d9941c473ac21354b4b
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-06-29 18:56:07 +0500

    Example Configuration

---
+++ src/EducationApp.DataAccess/Database/Persistence/Configuration/ExcampleConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using EducationApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EducationApp.DataAccess.Database.Persistence.Configuration;
+
+public class ExcampleConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
