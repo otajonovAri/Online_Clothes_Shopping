8f127f7b76468b2b3eb7d7f4d170fa072b123500
OneWay2Go <uchqunov.m@yandex.com>
2025-07-03 19:15:35 +0500

    Dtos corrected

--- src/EducationApp.Core/DTOs/StaffDto.cs
+++ src/EducationApp.Core/DTOs/StaffDto.cs
@@ -8,7 +8,7 @@ public class StaffDto
     public string FullName { get; set; }
     public string Position { get; set; }
     public string PhoneNumber { get; set; }
-    public string Address { get; set; }
+    public string Email { get; set; }
     public DateTime BirthDate { get; set; }
     public Gender Gender { get; set; }
     public decimal Salary { get; set; }
