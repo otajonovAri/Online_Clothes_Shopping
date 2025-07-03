00aa42c8d8ef0af8042080d96a75a5291461f547
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-07-03 11:31:12 +0500

    updated

--- src/EducationApp.API/Program.cs
+++ src/EducationApp.API/Program.cs
@@ -21,11 +21,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
