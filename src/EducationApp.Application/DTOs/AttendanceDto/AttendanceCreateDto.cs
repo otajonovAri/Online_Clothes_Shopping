c76d5d8711b3d31e68b2caa2e6ae716f465cdf15
OneWay2Go <uchqunov.m@yandex.com>
2025-07-29 20:04:48 +0500

    dto

--- src/EducationApp.Application/DTOs/AttendanceDto/AttendanceCreateDto.cs
+++ src/EducationApp.Application/DTOs/AttendanceDto/AttendanceCreateDto.cs
@@ -7,6 +7,7 @@ public class AttendanceCreateDto
 {
     [Required] public int StudentId { get; set; }
     [Required] public int GroupSubjectId { get; set; }
+    [Required] public int GroupId { get; set; }
     [Required] public DateTime AttendanceDate { get; set; }
     [Required] public bool IsPresent { get; set; }
     public PreparednessLevel? Preparedness { get; set; }
